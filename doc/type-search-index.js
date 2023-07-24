774e5d7f1c327457fc989b51a4c771304c079d15
FerAnzArt <130519612+FerAnzArt@users.noreply.github.com>
2023-07-23 23:36:51 -0600

    Delete doc directory

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"mx.com.conversor.modelo","l":"ExceptionMoneda"},{"p":"mx.com.conversor.modelo","l":"MenuInicial"},{"p":"mx.com.conversor.modelo","l":"Registro"}];updateSearchResults();
